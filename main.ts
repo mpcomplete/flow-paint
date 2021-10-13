ac0463e952b02999bdc711facd8ae1583eef293a
Matt Perry <mpcomplete@gmail.com>
2021-10-12 20:42:04 -0400

    Add simplex and sinusoid flow types

--- main.ts
+++ main.ts
@@ -10,6 +10,7 @@ type Point = [number, number];
 
 var config:any = {
   numParticles: 9000,
+  clear: () => clearScreen(),
 };
 window.onload = function() {
   let gui = new dat.GUI();
@@ -22,10 +23,12 @@ window.onload = function() {
   addConfig('lineWidth', 0.5, 0.2, 20.0).step(.01);
   addConfig('lineLength', 0.1, 0.02, 1.0).step(.01);
   addConfig('lineSpeed', 1., 0.1, 2.0).step(.1);
-  addConfig('wiggles', 3., 0., 5.).step(1);
-  addConfig('flowType', 'voronoi').options(['voronoi', 'smooth noise']);
+  addConfig('variance', 1., 0.1, 3.).step(.1);
+  addConfig('jaggies', 3., 0., 5.).step(1);
+  addConfig('flowType', 'voronoi').options(['voronoi', 'fractal', 'simplex', 'sinusoid']);
   addConfig('varyFlowField', true);
   addConfig('showFlowField', true);
+  gui.add(config, 'clear');
   initFramebuffers();
   dragdrop.init();
   dragdrop.handlers.ondrop = function(url) {
@@ -84,7 +87,10 @@ function initFramebuffers() {
 
 function initGenerator(opts) {
   sourceImageGenerator = imageGenerator(regl, [reglCanvas.width, reglCanvas.height], opts);
+  clearScreen();
+}
 
+function clearScreen() {
   let ctxDst = screenCanvas.dst.getContext('2d') as CanvasRenderingContext2D;
   ctxDst.clearRect(0, 0, screenCanvas.dst.width, screenCanvas.dst.height);
 }
@@ -122,11 +128,19 @@ precision highp sampler2D;
 
 struct Options {
   bool useVoronoi;
-  float wiggles;
+  bool useFBM;
+  bool useSimplex;
+  float jaggies;
+  float variance;
 };
 
 float PI = 3.14159269369;
 float TAU = 6.28318530718;
+
+vec2 rotate(vec2 p, float angle) {
+  return mat2(cos(angle), -sin(angle),
+              sin(angle), cos(angle)) * p;
+}
 
 // http://www.jcgt.org/published/0009/03/02/
 uvec3 pcg3d(uvec3 v) {
@@ -149,6 +163,8 @@ vec3 hash3(vec3 uvt) {
   uvec3 hu = pcg3d(uvec3(uvt * 1717.));  // scale by approximate resolution
   return vec3(hu) * (1.0/float(0xffffffffu));
 }
+
+// Basic 3D noise
 // https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
 vec3 noise3(vec3 x) {
 	vec3 i = floor(x);
@@ -159,50 +175,76 @@ vec3 noise3(vec3 x) {
              mix(mix( hash3(i + vec3(0,0,1)), hash3(i + vec3(1,0,1)), u.x),
                  mix( hash3(i + vec3(0,1,1)), hash3(i + vec3(1,1,1)), u.x), u.y), u.z);
 }
-vec2 rotate(vec2 _st, float _angle) {
-  return mat2(cos(_angle), -sin(_angle),
-              sin(_angle), cos(_angle)) * _st;
+
+// Simplex Noise
+// https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
+const float F3 = 0.3333333;
+const float G3 = 0.1666667;
+float snoise(vec3 p) {
+	vec3 s = floor(p + dot(p, vec3(F3)));
+	vec3 x = p - s + dot(s, vec3(G3));
+
+	vec3 e = step(vec3(0.0), x - x.yzx);
+	vec3 i1 = e*(1.0 - e.zxy);
+	vec3 i2 = 1.0 - e.zxy*(1.0 - e);
+
+	vec3 x1 = x - i1 + G3;
+	vec3 x2 = x - i2 + 2.0*G3;
+	vec3 x3 = x - 1.0 + 3.0*G3;
+
+	vec4 w, d;
+
+	w.x = dot(x, x);
+	w.y = dot(x1, x1);
+	w.z = dot(x2, x2);
+	w.w = dot(x3, x3);
+
+	w = max(0.6 - w, 0.0);
+
+	d.x = dot(hash3(s), x);
+	d.y = dot(hash3(s + i1), x1);
+	d.z = dot(hash3(s + i2), x2);
+	d.w = dot(hash3(s + 1.0), x3);
+
+	w *= w;
+	w *= w;
+	d *= w;
+
+	return .5 + .5*dot(d, vec4(52.0));
 }
-vec2 randomPoint(vec2 uv, float t) {
-  return hash3(vec3(uv, t)).xy;
+vec2 snoise2(vec3 p) {
+  return vec2(snoise(p+vec3(17.1)), snoise(p+vec3(3.7)));
 }
-vec3 hsv2rgb(vec3 c) {
-  // Íñigo Quílez
-  // https://www.shadertoy.com/view/MsS3Wc
-  vec3 rgb = clamp(abs(mod(c.x*6.+vec3(0.,4.,2.),6.)-3.)-1.,0.,1.);
-  rgb = rgb * rgb * (3. - 2. * rgb);
-  return c.z * mix(vec3(1.), rgb, c.y);
-}
-float noise(vec3 p) {
-  return noise3(p).x;
-}
+
+// Fractional Brownian Motion
 // Íñigo Quílez
-const mat2 m = mat2( 0.80,  0.60, -0.60,  0.80 );
+const mat2 m = mat2(0.80,  0.60, -0.60,  0.80);
 float fbm(vec3 p) {
   float f = 0.0;
-  f += 0.500000*noise( p ); p.xy = m*p.xy*2.02;
-  f += 0.250000*noise( p ); p.xy = m*p.xy*2.03;
-  f += 0.125000*noise( p ); p.xy = m*p.xy*2.01;
-  f += 0.062500*noise( p ); p.xy = m*p.xy*2.04;
-  f += 0.031250*noise( p ); p.xy = m*p.xy*2.01;
-  f += 0.015625*noise( p );
+  f += 0.500000*snoise(p); p.xy = m*p.xy*2.02;
+  f += 0.250000*snoise(p); p.xy = m*p.xy*2.03;
+  f += 0.125000*snoise(p); p.xy = m*p.xy*2.01;
+  f += 0.062500*snoise(p); p.xy = m*p.xy*2.04;
+  f += 0.031250*snoise(p); p.xy = m*p.xy*2.01;
+  f += 0.015625*snoise(p);
   return f/0.96875;
 }
 vec2 fbm2(vec3 p) {
   return vec2(fbm(p+vec3(16.8)), fbm(p+vec3(11.5)));
 }
 
-vec2 voronoi(vec2 st, float t, Options options) {
-  vec2 i_st = floor(st);
-  vec2 f_st = fract(st);
+// Voronoi noise
+vec2 voronoi(vec2 p, float t, Options options) {
+  vec2 i = floor(p);
+  vec2 f = fract(p);
 
   float minDist = 1.;
   vec2 v = vec2(1., 0.);
   for (int y = -1; y <= 1; y++) {
     for (int x = -1; x <= 1; x++) {
       vec2 cell = vec2(float(x), float(y));
-      vec2 cellCenter = noise3(vec3(i_st + cell, t*.1)).xy;
-      vec2 diff = cell + cellCenter - f_st;
+      vec2 cellCenter = snoise2(vec3(i + cell, t));
+      vec2 diff = cell + cellCenter - f;
       float dist = dot(diff, diff);
       if (dist < minDist) {
         dist = minDist;
@@ -210,22 +252,30 @@ vec2 voronoi(vec2 st, float t, Options options) {
       }
     }
   }
-  float a = 0.;
-  if (options.wiggles > 0.)
-    a = noise3(vec3(st*options.wiggles*5., t)).x*TAU/4.;
-  return rotate(normalize(v), PI/2. + a);
+  return rotate(normalize(v), PI/2.);
 }
-vec2 velocityAtPoint(vec2 p, float t, Options options) {
-  p *= 7.;
 
-  if (options.useVoronoi)
-    return voronoi(p, t*.3, options);
-  // vec2 v = noise3(vec3(p,0.)).xy - .5;
-  // v = noise3(vec3(dFdx(v), 0.)).xy - .5;
-  // v = vec2(dFdx(v).x, dFdy(v).y);
-  vec2 v = fbm2(vec3(p*1.3, t*.1)) - .5;
-  float a = (noise3(vec3(p*options.wiggles*11., t*.02)).x - .5)*TAU/8.;
-  return rotate(normalize(v), a*0.);
+vec2 velocityAtPoint(vec2 p, float t, Options options) {
+  t = snoise(vec3(t*.03, 0, 0));
+  p *= options.variance;
+  vec2 v = vec2(1., 0.);
+  if (options.useVoronoi) {
+    v = voronoi(p*7., t, options);
+  } else if (options.useFBM) {
+    v = fbm2(vec3(p*2., t)) - .5;
+  } else if (options.useSimplex) {
+    v = snoise2(vec3(p*5., t)) - .5;
+  } else {
+    float th = (t - .5)*.4;
+    vec2 pr = p * TAU/4.;
+    v.x = sin(TAU * sin(pr.x*1.7) * sin(pr.y*3.1) + (pr.x-.1 + th)*(pr.y+.2)*TAU);
+    pr.y += sin(pr.x);
+    v.y = sin(1.3 + TAU * sin(pr.x*4.5) * sin(pr.y*1.3) + (pr.x+.1)*(pr.y-.4 + th)*TAU*.7);
+  }
+  float a = 0.;
+  if (options.jaggies > 0.)
+    a = snoise(vec3(p*options.jaggies*11., t*3.)) - .5;
+  return rotate(normalize(v), a * TAU/4.);
 }`;
 
 const baseVertShader = (opts) => regl(Object.assign({
@@ -260,6 +310,9 @@ const updateParticles = baseVertShader({
   uniform vec2 iResolution;
   uniform Options options;
 
+  vec2 randomPoint(vec2 uv, float t) {
+    return hash3(vec3(uv, t)).xy;
+  }
   void maybeReset(inout vec2 pos, inout vec2 newPos) {
     float birth = texelFetch(birthTex, ivec2(gl_FragCoord.xy), 0).x;
     float death = maxAge*(1. + .5*hash3(vec3(gl_FragCoord.xy/iResolution.xy + pos, clockTime)).x);
@@ -286,7 +339,10 @@ const updateParticles = baseVertShader({
     iTime: () => animateTime,
     iResolution: (context) => [context.viewportWidth, context.viewportHeight],
     'options.useVoronoi': () => config.flowType == 'voronoi',
-    'options.wiggles': () => config.wiggles,
+    'options.useFBM': () => config.flowType == 'fractal',
+    'options.useSimplex': () => config.flowType == 'simplex',
+    'options.jaggies': () => config.jaggies,
+    'options.variance': () => config.variance,
   },
 });
 
@@ -312,7 +368,10 @@ const drawFlowField = baseVertShader({
   uniforms: {
     iTime: () => animateTime,
     'options.useVoronoi': () => config.flowType == 'voronoi',
-    'options.wiggles': () => config.wiggles,
+    'options.useFBM': () => config.flowType == 'fractal',
+    'options.useSimplex': () => config.flowType == 'simplex',
+    'options.jaggies': () => config.jaggies,
+    'options.variance': () => config.variance,
   },
 });
 
